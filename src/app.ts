d9dcd1d6a88a3d0c315ff2af43fb031a95c853f3
Lost <pandagaminggr@gmail.com>
2023-03-28 14:31:26 +0300

    chore: remove server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,1 @@
-import express from "express";
-import morgan from "morgan";
-
-// TODO: Setup API Endpoints
-
-const app = express();
-const port = 3000;
-
-app.use(morgan("dev"));
-
-app.get("/chat", (req, res) => {
-	res.send("WIP");
-});
-
-app.listen(port, () => {
-	console.log(`Listening on port ${port}`);
-});
+// TODO: Implement API
