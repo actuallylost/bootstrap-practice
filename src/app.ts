3b583993190bae23b40ea82a5788ca73bbd06291
Lost <pandagaminggr@gmail.com>
2023-03-28 14:30:57 +0300

    feat: implement return to home button

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import morgan from "morgan";
+
+// TODO: Setup API Endpoints
+
+const app = express();
+const port = 3000;
+
+app.use(morgan("dev"));
+
+app.get("/chat", (req, res) => {
+	res.send("WIP");
+});
+
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
